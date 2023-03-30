3a3817886155ddef2b0e1cfa7c65a30846d5a306
y-mhiri <yassine.mhiri@outlook.fr>
2023-03-30 12:38:08 +0000

    simulate command tested

--- robii/commands/generate_dataset.py
+++ robii/commands/generate_dataset.py
@@ -1,5 +1,7 @@
 from ..simulation.simulation import ViSim
+from ..simulation.rfi import RFI
 from ..astro.mstozarr import save_telescope_raw
+
 import click 
 
 
@@ -11,25 +13,30 @@ def generate_dataset():
     pass
 
 @generate_dataset.command()
-@click.option('--ndata', default=1000, help='Number of data to generate')
+@click.option('--ndata', default=10, help='Number of data to generate')
 @click.option('--telescope', default='vla', help='Telescope name')
-@click.option('--npixel', default=512, help='Number of pixels')
-@click.option('--snr', default=10, help='Signal to noise ratio')
-@click.option('--texture_distributions', default='gaussian', help='Texture distributions')
-@click.option('--dof_ranges', default='(0,1)', help='Degrees of freedom ranges')
-@click.option('--add_noise', default=False, help='Add noise')
-@click.option('--add_compound', default=False, help='Add compound')
-@click.option('--add_rfi', default=False, help='Add rfi')
-@click.option('--add_calibration_error', default=False, help='Add calibration error')
+@click.option('--npixel', default=32, help='Number of pixels')
+@click.option('--snr', default=10.0, help='Signal to noise ratio')
+@click.option('--texture_distributions', default=['invgamma', 'gamma', 'invgauss'], help='Texture distributions', multiple=True)
+@click.option('--dof_ranges', default=[(5.0,10.0)], help='Degrees of freedom ranges', multiple=True)
+@click.option('--add_noise/--no_noise', default=False, help='Add noise')
+@click.option('--add_compound/--no_compound', default=False, help='Add compound')
+@click.option('--add_rfi/--no_rfi', default=False, help='Add rfi')
+@click.option('--add_calibration_error/--no_calibration_error', default=False, help='Add calibration error')
 @click.option('--std_calibration_error', default=0.05, help='Standard deviation of calibration error')
-@click.option('--rfi_array', default='rfi_array.npy', help='Rfi array')
-@click.option('--nvis', default=1000, help='Number of visibilities')
-@click.option('--freq', default=1000, help='Frequency')
+@click.option('--rfi_power', default=[1.0], help='Rfi array', multiple=True)
+@click.option('--freq', default=3.0e8, help='Frequency')
 @click.option('--out', default='dataset.zarr', help='Output path')
-def simulate(ndata, telescope, npixel, snr, texture_distributions, dof_ranges, add_noise, add_compound, add_rfi, add_calibration_error, std_calibration_error, rfi_array, nvis, freq, out):
+def simulate(ndata, telescope, npixel, snr, texture_distributions, dof_ranges, add_noise, add_compound, add_rfi, add_calibration_error, std_calibration_error, rfi_power, freq, out):
     """
     Generate a dataset
     """
+
+    print(texture_distributions)
+    print(dof_ranges)
+
+    rfi_array = [RFI(power) for power in rfi_power]
+
     sim = ViSim(ndata=ndata,
                 telescope=telescope,
                 npixel=npixel,
@@ -42,7 +49,6 @@ def simulate(ndata, telescope, npixel, snr, texture_distributions, dof_ranges, a
                 add_calibration_error=add_calibration_error,
                 std_calibration_error=std_calibration_error,
                 rfi_array=rfi_array,
-                nvis=nvis,
                 freq=freq)
     
     sim.save_as_zarr(out)
