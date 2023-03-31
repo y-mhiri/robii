26c525f6d45797298e39205bb2f33c4b86e94277
y-mhiri <yassine.mhiri@outlook.fr>
2023-03-31 07:53:31 +0000

    cllesize added to VIdataset class

--- robii/deep/datasets.py
+++ robii/deep/datasets.py
@@ -16,12 +16,12 @@ class ViDataset():
         self.z = zarr.group(store=store)
 
         metadata = dict(self.z["metadata"])
-        print(metadata.keys())
         self.nimages = metadata["nimage"].astype(int)
         self.npixel = metadata["npixel"].astype(int)
         self.nvis = len(self.z["data/uvw"])
         self.uvw = np.array(self.z['data/uvw']).squeeze()     
         self.freq = np.array(self.z['data/freq']).reshape(-1)
+        self.cellsize = metadata["cellsize"].astype(float)
 
     def __getitem__(self,idx):
 
