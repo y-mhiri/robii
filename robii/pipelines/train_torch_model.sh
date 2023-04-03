448e739635e26adcede7a59617f8f2767f3d76b4
y-mhiri <yassine.mhiri@outlook.fr>
2023-04-03 12:58:33 +0200

    updated with param used for GRETSI results

--- robii/pipelines/train_torch_model.sh
+++ robii/pipelines/train_torch_model.sh
@@ -2,7 +2,7 @@
 
 
 output_path=/workdir/mhiriy/robii/outputs
-folder_name=vla_8h_1h_nonoise
+folder_name=GRETSI
 
 out=$output_path/$folder_name
 mkdir $out
@@ -16,22 +16,22 @@ mkdir log
 
 # Generate train datasets 
 generate_dataset simulate --ndata 128 \
---telescope vla \
---synthesis 8 \
---dtime 3600 \
+--telescope random_static \
+--synthesis 0 \
+--dtime 0 \
 --dec 'zenith' \
---npixel 128 \
+--npixel 64 \
 --out $datasets_path/train \
 --freq 1.4e9 \
-# --add_noise \
-# --snr 20 \
-# --add_compound \
-# --texture_distributions invgamma \
-# --dof_ranges 3 10 \
-# --texture_distributions gamma \
-# --dof_ranges .1 5 \
-# --texture_distributions invgauss \
-# --dof_ranges .5 1
+--add_noise \
+--snr 20 \
+--add_compound \
+--texture_distributions invgamma \
+--dof_ranges 3 10 \
+--texture_distributions gamma \
+--dof_ranges .1 5 \
+--texture_distributions invgauss \
+--dof_ranges .5 1
 
 
 # train the model
@@ -46,61 +46,4 @@ train_model --dset_path $datasets_path/train.zip \
 --logpath $out/log/log.out \
 --true_init
 
-# # generate test dataset
-
-# generate_dataset simulate --ndata 1000 \
-# --telescope random_static \
-# --npixel 64 \
-# --out $datasets_path/test \
-# --freq 3.0e8 \
-# --add_noise \
-# --snr 20 \
-# --add_compound \
-# --texture_distributions invgamma \
-# --dof_ranges 2.5 7 \
-# --texture_distributions gamma \
-# --dof_ranges .1 1 \
-# # --texture_distributions invgauss \
-# # --dof_ranges .5 1
-
-
-
-# make example image
-
-# robiinet fromzarr $datasets_path/test.zip \
-# -n 10 \
-# -o $output_path/test_output/images \
-# --niter 10 \
-# --miter 100 \
-# --mstep_size .1 \
-# --threshold 0.001 \
-# --model_path $output_path/train_output/robiinet.pth
-
-
-# # test the model
-# test_model --dset_path $datasets_path/test.zip \
-# --mstep_size .1 \
-# --niter 10 \
-# --miter 100 \
-# --threshold 0.001 \
-# --out $output_path/test_output \
-# --model_path $output_path/train_output/robiinet.pth \
-# --logpath $output_path/log 
-
-
-
-# maker image from real data
-
-# mspath=/Users/ymhiri/Documents/Dev/unrolled-robust-imaging/robii/data/ms
-
-# robiinet fromms $mspath/SNR_G55_10s.calib.ms \
-# --out $output_path/test_output/real_data/ \
-# --model_path $output_path/train_output/robiinet.pth \
-# --niter 10 \
-# --miter 1 \
-# --mstep_size 0.0005 \
-# --threshold 0.00025 \
-# --image_size 32 \
-# --cellsize 8  \
-# --fits True 
 
