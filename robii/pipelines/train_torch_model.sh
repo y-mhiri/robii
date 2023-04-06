0dc19b3785e5943ed5aecc079d320b89ab89fa42
y-mhiri <yassine.mhiri@outlook.fr>
2023-04-06 19:00:14 +0000

    up to date bash script

--- robii/pipelines/train_torch_model.sh
+++ robii/pipelines/train_torch_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-output_path=/workdir/mhiriy/robii/outputs
-folder_name=vla-snr$1-n$2-t$3-m$4-lr$5-snr$6
+output_path=/synced/robii/outputs
+folder_name=vla_test
 
 out=$output_path/$folder_name
 mkdir $out
@@ -17,36 +17,35 @@ mkdir train_output
 mkdir log
 
 # Generate train datasets 
-generate_dataset simulate --ndata 1024 \
---telescope vla \
---synthesis 0 \
---dtime 0 \
---dec 'zenith' \
---npixel 128 \
---out $datasets_path/train \
---freq 1.4e9 \
---add_noise \
---snr $1 \
---add_compound \
---texture_distributions invgamma \
---dof_ranges 3 10 \
---texture_distributions gamma \
---dof_ranges .1 5 \
---texture_distributions invgauss \
---dof_ranges .5 1
+# generate_dataset simulate --ndata 1024 \
+# --telescope vla \
+# --synthesis 8 \
+# --dtime 3600 \
+# --dec 'zenith' \
+# --npixel 128 \
+# --out $datasets_path/train \
+# --freq 1.4e9 \
+# --add_noise \
+# --snr 20 \
+# --add_compound \
+# --texture_distributions invgamma \
+# --dof_ranges 3 10 \
+# --texture_distributions gamma \
+# --dof_ranges .1 5 \
+# --texture_distributions invgauss \
+# --dof_ranges .5 1
 
 
 # train the model
 train_model --dset_path $datasets_path/train.zip \
 --nepoch 100 \
---batch_size 16 \
---net_depth $2  \
+--batch_size 64 \
+--net_depth 10  \
 --net_width 351 \
---threshold $3 \
---mstep_size $4 \
---learning_rate $5 \
+--threshold $1 \
+--mstep_size $2 \
+--learning_rate $3 \
 --step 10 \
---snr $6
 --out $out/train_output \
 --model_name robiinet \
 --logpath $out/log/log.out \
