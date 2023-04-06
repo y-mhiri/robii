4bf39f7e4d049cd38076beaca7324403aadfe4e3
y-mhiri <yassine.mhiri@outlook.fr>
2023-04-06 08:58:01 +0200

    up to date bash files

--- robii/pipelines/test_torch_model.sh
+++ robii/pipelines/test_torch_model.sh
@@ -2,7 +2,7 @@
 
 
 output_path=/workdir/mhiriy/robii/outputs
-folder_name=GRETSI
+folder_name=vla-snr100-n10-t.001-m.001-lr0.001
 
 out=$output_path/$folder_name
 cd $out
@@ -14,43 +14,47 @@ datasets_path=$out/datasets
 
 # Generate test dataset
 
-generate_dataset simulate --ndata 1000 \
---telescope random_static \
---npixel 64 \
---out $datasets_path/test \
---freq 3.0e8 \
---add_noise \
---snr 20 \
---add_compound \
---texture_distributions invgamma \
---dof_ranges 3 10 \
---texture_distributions gamma \
---dof_ranges .1 1 \
---texture_distributions invgauss \
---dof_ranges .5 1
+# generate_dataset simulate --ndata 10 \
+# --telescope vla \
+# --synthesis 8 \
+# --dtime 3600 \
+# --dec 'zenith' \
+# --npixel 128 \
+# --out $datasets_path/test \
+# --freq 1.4e9 \
+# --add_noise \
+# --snr 100 \
+# --add_compound \
+# --texture_distributions invgamma \
+# --dof_ranges 3 10 \
+# --texture_distributions gamma \
+# --dof_ranges .1 5 \
+# --texture_distributions invgauss \
+# --dof_ranges .5 1
+
 
 
 # make example image
 
 robiinet fromzarr $datasets_path/test.zip \
--n 10 \
--o $out/test_output/images \
+--nimages 10 \
+--out $out/test_output/images \
 --niter 10 \
---miter 1 \
---mstep_size 1 \
---threshold 0.1 \
---model_path $out/train_output/robiinet.pth
+--miter 10 \
+--mstep_size .001 \
+--threshold 0.001 \
+--model_path $out/train_output/robiinet_tmp.pth
 
 
 # test the model
-test_model --dset_path $datasets_path/test.zip \
---mstep_size .1 \
---niter 10 \
---miter 100 \
---threshold 0.001 \
---out $out/test_output \
---model_path $out/train_output/robiinet.pth \
---logpath $out/log 
+# test_model --dset_path $datasets_path/test.zip \
+# --mstep_size .1 \
+# --niter 10 \
+# --miter 100 \
+# --threshold 0.001 \
+# --out $out/test_output \
+# --model_path $out/train_output/robiinet.pth \
+# --logpath $out/log 
 
 
 
--- robii/pipelines/train_torch_model.sh
+++ robii/pipelines/train_torch_model.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-output_path=/Users/ymhiri/Documents/Dev/unrolled-robust-imaging/outputs
-folder_name=vla-test
+output_path=/workdir/mhiriy/robii/outputs
+folder_name=vla-snr$1-n$2-t$3-m$4-lr$5-snr$6
 
 out=$output_path/$folder_name
 mkdir $out
+cp train_torch_model.sh $out
+
 cd $out
 
 mkdir datasets
@@ -15,16 +17,16 @@ mkdir train_output
 mkdir log
 
 # Generate train datasets 
-generate_dataset simulate --ndata 128 \
+generate_dataset simulate --ndata 1024 \
 --telescope vla \
---synthesis 1 \
---dtime 3600 \
+--synthesis 0 \
+--dtime 0 \
 --dec 'zenith' \
 --npixel 128 \
 --out $datasets_path/train \
 --freq 1.4e9 \
 --add_noise \
---snr 20 \
+--snr $1 \
 --add_compound \
 --texture_distributions invgamma \
 --dof_ranges 3 10 \
@@ -38,10 +40,13 @@ generate_dataset simulate --ndata 128 \
 train_model --dset_path $datasets_path/train.zip \
 --nepoch 100 \
 --batch_size 16 \
---net_depth 10  \
+--net_depth $2  \
 --net_width 351 \
---learning_rate 0.0001 \
+--threshold $3 \
+--mstep_size $4 \
+--learning_rate $5 \
 --step 10 \
+--snr $6
 --out $out/train_output \
 --model_name robiinet \
 --logpath $out/log/log.out \
