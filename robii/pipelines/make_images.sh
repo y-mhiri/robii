c2e58265399ef5b17010f036b2775c0e735d550b
y-mhiri <yassine.mhiri@outlook.fr>
2023-04-07 14:45:17 +0200

    up to date bash script

--- robii/pipelines/make_images.sh
+++ robii/pipelines/make_images.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-
-
-
-
-
-robii fromms SNR_G55_10s.calib.ms 
--o SNR_G55 
---image_size 512 
---niter 10 
---miter 10
---mstep_size 0.0005 
---threshold .00025 
+robii fromms SNR_G55_10s.calib.ms \
+--out SNR_G55 \
+--image_size 512 \
+--niter 10 \
+--miter 10 \
+--mstep_size 0.0005 \
+--threshold .00025 \
 --dof 1
+--plot
