b106a61f125352248797b76a5c8eaafb1a5b7712
y-mhiri <yassine.mhiri@outlook.fr>
2023-05-03 18:27:47 +0200

    skymodel class to simulate real sky images

--- robii/simulation/sky_model.py
+++ robii/simulation/sky_model.py
@@ -3,18 +3,22 @@ import numpy as np
 
 
 class Source():
-    def __init__(self, center, power, scale):
+    def __init__(self, center, power, scale=None, component='point_source'):
 
         self.center = center # in degree (l, m) are cosine direction from the phase center
         self.power = power # in Jy
         self.scale = scale # in degree
 
+        if component not in ['gaussian', 'point_source']:
+            raise ValueError('component must be gaussian or point_source')
+        
+        self.component = component
+
     
 
 class SkyModel():
 
-    def __init__(self, size, cellsize, src_density=13000, scale_min=5, scale_max=20,
-                 add_noise=False, rng=np.random.default_rng()):
+    def __init__(self, size, cellsize):
 
         if type(size) is int:
             self.npix_x = size
@@ -37,51 +41,82 @@ class SkyModel():
         
 
         self.fov = (self.npix_x*self.cellsize_x, self.npix_y*self.cellsize_y)
-        self.src_density = src_density # number of sources per square degree
-        self.scale_min = scale_min # in degree 
-        self.scale_max = scale_max # in degree
-        self.add_noise = add_noise
-        self.rng = rng
+
 
         # self.skymodel = self.generate_sky_model()
 
-    def generate_sky_model(self, sources=None):
+    def generate_sky_model(self, sources=None, src_density=None, scale_min=None, scale_max=None, component='point_source', add_noise=False, std=1, rng= np.random.default_rng()):
 
         if sources is None:
-            nsources = self.rng.poisson(self.src_density* (self.fov[0]*self.fov[1]))
+            nsources = rng.poisson(src_density* (self.fov[0]*self.fov[1]))
             print( 'Number of sources: ', nsources)
             sources = []
             for _ in range(nsources):
-                center_x = self.rng.uniform(-self.fov[0]/2, self.fov[0]/2)
-                center_y = self.rng.uniform(-self.fov[1]/2, self.fov[1]/2)
+
+                center_x = rng.uniform(0, self.fov[0]/2) * rng.choice([-1, 1]) # done this way to avoid the the extreme edges of the fov
+                center_y = rng.uniform(0, self.fov[0]/2) * rng.choice([-1, 1])
                 center = (center_x, center_y)
 
-                power = self.rng.uniform(1, 10)
-                scale = self.rng.uniform(self.scale_min, self.scale_max, size=2)
-                sources.append(Source(center, power, scale))
 
-        sky_model = np.zeros((self.npix_x, self.npix_y))
+                power = rng.uniform(1, 10)
+                if component == 'gaussian':
+                    scale = rng.uniform(scale_min, scale_max, size=2)
+                    sources.append(Source(center, power, scale, component=component))
+                elif component == 'point_source':
+                    sources.append(Source(center, power, component=component))
+                else:
+                    raise ValueError('component must be gaussian or point_source')
+
+                
+
+        sky_image = np.zeros((self.npix_x, self.npix_y))
+        self.nsources = len(sources)
 
         for s in sources:
             source_center = (s.center[0]/self.cellsize_x , s.center[1]/self.cellsize_y)
             source_power = s.power
-            source_scale = (s.scale[0]/self.cellsize_x, s.scale[1]/self.cellsize_y)
-            sky_model += self.ellipsoid(source_center, source_power, source_scale, (self.npix_x, self.npix_y))
+            component = s.component
 
-        if self.add_noise:
-            std = np.sqrt(np.sum(sky_model**2)/nsources)/100
-            sky_model += self.rng.normal(0, std, size=(self.npix_x, self.npix_y))
+            if component == 'gaussian':
+                source_scale = (s.scale[0]/self.cellsize_x, s.scale[1]/self.cellsize_y)
+                print(source_center, source_power, source_scale)
+                sky_image += self.ellipsoid(source_center, source_power, source_scale, (self.npix_x, self.npix_y))
+            elif component == 'point_source':
+                sky_image += self.point_source(source_center, source_power, (self.npix_x, self.npix_y))
+
+        if add_noise:
+            sky_image += self.rng.normal(0, std, size=(self.npix_x, self.npix_y))
         
-        return sky_model
+
+        return sky_image
     
 
-    def ellipsoid(self, center, P, scale, size):
+    def ellipsoid(self, center_px, P, scale_px, size):
 
         npix_x, npix_y = size
 
         X,Y = np.meshgrid(np.linspace(-npix_x//2, npix_x//2, npix_x),np.linspace(-npix_y//2, npix_y//2, npix_y))
-        ellipsoid_image = P * np.exp(-((X-center[0]*np.ones_like(X))**2/scale[0] + (Y-center[1]*np.ones_like(Y))**2/scale[1]))
+        ellipsoid_image = P * np.exp(-((X-center_px[0]*np.ones_like(X))**2/scale_px[0]**2 + (Y-center_px[1]*np.ones_like(Y))**2/scale_px[1]**2))
         return ellipsoid_image
+    
+
+    def point_source(self, center, P, size):
+
+        npix_x, npix_y = size
+
+        point_source_image = np.zeros((npix_x, npix_y))
+        point_source_image[np.round(center[0] + npix_x//2).astype(int), np.round(center[1] + npix_y//2).astype(int)] = P
+        return point_source_image
+
+
+def ellipsoid(center_px, P, scale_px, size):
+
+    npix_x, npix_y = size
+
+    X,Y = np.meshgrid(np.linspace(-npix_x//2, npix_x//2, npix_x),np.linspace(-npix_y//2, npix_y//2, npix_y))
+    ellipsoid_image = P * np.exp(-((X-center_px[0]*np.ones_like(X))**2/scale_px[0]**2 + (Y-center_px[1]*np.ones_like(Y))**2/scale_px[1]**2))
+    return ellipsoid_image
+
 
 def generate_sky_model(npixel, src_density=.5, scale_min=5, scale_max=20,
                        sources=None, add_noise=False, rng=np.random.default_rng()):
@@ -104,7 +139,7 @@ def generate_sky_model(npixel, src_density=.5, scale_min=5, scale_max=20,
         source_center = s.center
         source_power = s.power
         source_scale = s.scale
-        skymodel += ellipsoid(source_center, source_power, source_scale, npixel)
+        skymodel += ellipsoid(source_center, source_power, source_scale, (npixel, npixel))
 
     if add_noise:
         std = np.sqrt(np.sum(skymodel**2)/nsources)/100
