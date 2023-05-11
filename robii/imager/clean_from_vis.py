dcf7a461412fa88d09de5319466a450f197f9d89
y-mhiri <yassine.mhiri@outlook.fr>
2023-05-11 21:02:20 +0200

    init not taking into account/ applying weights if given

--- robii/imager/clean_from_vis.py
+++ robii/imager/clean_from_vis.py
@@ -2,19 +2,29 @@ import numpy as np
 from ..arl.clean import deconvolve_cube
 
 
-def clean_from_vis(vis, ops, psf, params={},  init=None):
+def clean_from_vis(vis, ops, psf, weights=None, params={},  init=None):
     
+    if weights is None:
+        weights = np.ones_like(vis.flatten())
+    else:
+        weights = weights.flatten()
+        assert len(weights) == len(vis.flatten()) 
+
     forward, backward = ops
     # nvis, nfreq = vis.shape
 
     if init is None:
-        dirty = backward(vis.flatten())#/len(vis.flatten())
+        dirty = backward(np.multiply(vis.flatten(), weights))#/len(vis.flatten())
+        # dirty = backward(vis.flatten())#/len(vis.flatten())
     else:
-        dirty = backward(vis.flatten() - forward(init).flatten()) #/len(vis.flatten())
+        dirty = backward(np.multiply(vis.flatten(), weights)) #- backward(forward(init).flatten()) #/len(vis.flatten())
+        # dirty = backward(vis.flatten())#/len(vis.flatten())
 
 
     comp, residual = deconvolve_cube(dirty, np_psf=psf, params=params)
-    comp = comp.data.reshape(dirty.shape) + init if init is not None else comp.data.reshape(dirty.shape)
+    comp = comp.data.reshape(dirty.shape) # + init if init is not None else comp.data.reshape(dirty.shape)
+
+
 
     return comp
     
